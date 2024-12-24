5fe772b39b1f5a3a2d8e0d4b696ef123bee843cb
inagib21 <inagib21@gmail.com>
2024-12-23 20:41:28 -0500

    added config & funcs

---
+++ alpaca_config/keys.py
@@ -0,0 +1,7 @@
+config = {
+  'key-id': 'PKQC5QAN7IBBWW7URCGF',
+  'secret_key': 'AgkUWBuWVPzzWKKJ2iJ4nYQWNjE20S5WWj5wDdFq',
+ 'redpanda_brokers': 'localhost:9092, localhost:9093',
+ 'base_url': 'https://data.alpaca.markets/v1beta1/',
+ 'trade_api_base_url': 'https://paper-api.alpaca.markets/v2'
+}
